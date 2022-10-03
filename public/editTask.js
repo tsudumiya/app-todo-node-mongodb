95fe0b62f86328327ffca421336dfdcb8ee33f73
k-terao <62705494+tsudumiya@users.noreply.github.com>
2022-10-03 14:29:04 +0900

    add files

---
+++ public/editTask.js
@@ -0,0 +1,48 @@
+const taskIDDOM = document.querySelector('.task-edit-id');
+const taskNameDOM = document.querySelector('.task-edit-name');
+const editFormDOM = document.querySelector('.single-task-form');
+const formAlertDOM = document.querySelector('.form-alert');
+const taskCompletedDOM = document.querySelector('.task-edit-completed');
+
+const params = window.location.search;
+const id = new URLSearchParams(params).get('id');
+console.log(id);
+
+// 一つの特定のタスクを取得する
+const showTask = async () => {
+    try {
+        const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
+        const { _id, completed, name } = task;
+        taskIDDOM.textContent = _id;
+        taskNameDOM.value = name;
+        if (completed) {
+            taskCompletedDOM.checked = true;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+showTask();
+
+// タスクの編集
+editFormDOM.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    try {
+        const taskName = taskNameDOM.value;
+        taskCompleted = taskCompletedDOM.checked;
+        const { data: task } = await axios.patch(`/api/v1/tasks/${id}`, {
+            name: taskName,
+            completed: taskCompleted,
+        });
+        formAlertDOM.getElementsByClassName.display = 'block';
+        formAlertDOM.textContent = '編集に成功しました';
+        formAlertDOM.classList.add('text-success');
+    } catch (error) {
+        console.log(error);
+    }
+    setTimeout(() => {
+        formAlertDOM.style.display = 'none';
+        formAlertDOM.classList.remove('text-success');
+    }, 3000);
+});
